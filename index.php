eb2fddf86795614fae9c23a60926aa09c570ad29
atifnewcastle <atiframlan@gmail.com>
2023-05-25 11:38:12 +0800

    2nd commit

--- index.php
+++ index.php
@@ -2,30 +2,69 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Dashboard</title>
-<link rel="stylesheet" href="css/bootstrap.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </script>
-<script src="https://maxcdn.bootstrapcdn.colybootstrap/3.3.7/js/bootstrap.js"></script> 
-<style type="text/css">
-.wrapper {
-width: 650px;
-margin: 0 auto;
-.page-header h25
-margin-top: 0;
-}
-table tr td:last-child a{
-margin-right: 15px;
-}
-</style> <script type="text/javascript">
-§(document). ready(function(){
-$('[data-toggle="tooltip"]'). tooltip();
-});
-</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script> 
 </head>
 <body>
+    <div class="wrapper">
+        <div class="container">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="page-header clearfix">
+                            <h2 class="mt-2">
+                                Employee Details
+                                <a href="create.php" class="btn btn-success float-end">Add new employee</a>
+                            </h2>
+                            
+                        </div>
+                        <?php
+                        require_once 'config.php';
+
+                        $sql = "SELECT * FROM employees";
+                        $result = mysqli_query($link, $sql);
+                        if (mysqli_num_rows($result) > 0) {
+                            echo "<table class='table table-bordered table-striped'>";
+                            echo "<thead>";
+                            echo "<tr>";
+                            echo "<th>#</th>";
+                            echo "<th>Name</th>";
+                            echo "<th>Address</th>";
+                            echo "<th>Salary</th>";
+                            echo "<th>Action</th>";
+                            echo "</tr>";
+                            echo "</thead>";
+                            echo "<tbody>";
+
+                            //list all select data
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo "<tr>";
+                                echo "<td>" . $row["id"] . "</td>";
+                                echo "<td>" . $row["name"] . "</td>";
+                                echo "<td>" . $row["address"] . "</td>";
+                                echo "<td>" . $row["salary"] . "</td>";
+                                echo "<td>";
+                                echo "<a href='read.php?id=".$row["id"]."' title='view record' class='btn btn-info mx-1' >view</a>";
+                                echo "<a href='update.php?id=".$row['id']."' title='view record' class='btn btn-warning mx-1' >update</a>";
+                                echo "<a href='delete.php?id=".$row['id']."' title='view record' class='btn btn-danger mx-1' >delete</a>";
+                            }
+                            echo "</tbody>";
+                            echo "</table>";
+                        //Free result set
+                        mysqli_free_result($result);
+
+                        }
+                        //Close connection
+                        mysqli_close($link);
+?>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
